3428f5909e2d0d161e1f03f7d966a9989d0c5166
Vasko <b.vasko@abv.bg>
2021-05-04 12:39:47 +0300

    Finish

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const inputBox = document.querySelector(".inputField input");
 const addBtn = document.querySelector(".inputField button");
 const todoList = document.querySelector(".todoList");
+const deleteAllBtn = document.querySelector(".footer button")
 
 inputBox.onkeyup = ()=>{
     let userData = inputBox.value; //Getting user entered value
@@ -30,8 +31,10 @@ addBtn.onclick = ()=>{
     listArr.push(userData); //Pushing or adding user data
     localStorage.setItem("New Todo", JSON.stringify(listArr)) //Transforming js object into a json string
     showTasks(); //Calling showTasks function
+    addBtn.classList.remove("active");
 }
 
+//Function to add task list inside ul
 function showTasks(){
     let getLocalStorage = localStorage.getItem("New Todo"); //Getting localStorage
     if(getLocalStorage == null){ //if the olcalStorage is empty
@@ -41,12 +44,38 @@ function showTasks(){
     {
         listArr = JSON.parse(getLocalStorage); //Transforming json string into a js object
     }
+    const pendingNumb = document.querySelector(".pendingNumb");
+    pendingNumb.textContent = listArr.length; //Passing the length value in pendingNumb
+    if(listArr.length > 0) //If the list isnt empty, activate the button
+    {
+        deleteAllBtn.classList.add("active");
+    }
+    else
+    {
+        deleteAllBtn.classList.remove("active");
+    }
     let newLiTag = '';
     listArr.forEach((element, index) => {
-        newLiTag += `<li> ${element} <span><i class="fas fa-trash"></i></span></li>`;
+        newLiTag += `<li> ${element} <span onclick="deleteTask(${index})"; ><i class="fas fa-trash"></i></span></li>`;
     });
     todoList.innerHTML = newLiTag; //Adding new li tag inside ul tag
     inputBox.value = "";
 }
 
-//Ding dong
+//Delete task function
+function deleteTask(index){
+    let getLocalStorage = localStorage.getItem("New Todo");
+    listArr = JSON.parse(getLocalStorage); 
+    listArr.splice(index, 1); //Delete or remove the particular indexed li
+    //After removing the li update the local storage
+    localStorage.setItem("New Todo", JSON.stringify(listArr)) 
+    showTasks(); //Calling showTasks function
+}
+
+//Delete all tasks function
+deleteAllBtn.onclick = ()=>{
+    listArr = []; //Empty an array
+    //After deleting all tasks update the local storage
+    localStorage.setItem("New Todo", JSON.stringify(listArr)) 
+    showTasks(); //Calling showTasks function
+}
